e24e7ac5b7d51d63b4275b1e46db3af3928bf291
Dineshreddy28 <pelleti.d@northeastern.edu>
2024-12-10 04:16:26 -0500

    FullStack Project

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import AppRouter from './routes/routes';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <AppRouter />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
